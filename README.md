80142ecaeee26b6fad16b70e46e5cb3d0291a8d3
Stephen Gibson <stephen.gibson@curvedental.com>
2013-12-23 10:27:03 -0700

    Adding install script, updating readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is an init script for Nagios 4+ to work with Ubuntu
+All you need to do is grab the bash script, and run ./install_nagios. This will grab the init file contained here.
+
+You will need to supply a password near the end of the process for the nagiosadmin user.
---
+++ install_nagios.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+apt-get update
+apt-get install -y wget build-essential php5-gd wget libgd2-xpm libgd2-xpm-dev libapache2-mod-php5 apache2-utils daemon
+useradd nagios
+usermod -a -G nagcmd nagios
+wget http://prdownloads.sourceforge.net/sourceforge/nagios/nagios-4.0.2.tar.gz
+wget https://www.nagios-plugins.org/download/nagios-plugins-1.5.tar.gz
+tar -xzf nagios-4.0.2.tar.gz
+tar -xzf nagios-plugins-1.5.tar.gz
+cd nagios-4.0.2/
+./configure --with-nagios-group=nagios --with-command-group=nagcmd --with-mail=/usr/bin/sendmail
+make all
+make install
+make install-init
+make install-config
+make install-commandmode
+make install-webconf
+cd ../nagios-plugins-1.5
+./configure --with-nagios-user=nagios --with-nagios-group=nagios
+make
+make install
+mkdir /usr/local/nagios/var/rw
+chmod 777 /usr/local/nagios/var/rw/ -R
+htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
+cd /etc/init.d/
+rm nagios
+wget https://raw.github.com/stephengibsoncurve/ubuntu_nagios4_init/master/nagios
+chmod 755 nagios
+/etc/init.d/apache2 restart
+/etc/init.d/nagios restart
